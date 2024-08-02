be188ff4269963eb05c231f75dfd97034c0a3787
Atul Reny <117465987+AtulRy@users.noreply.github.com>
2024-08-03 02:31:38 +0530

    top banner done

--- lib/pages/domain.dart
+++ lib/pages/domain.dart
@@ -1,5 +1,7 @@
 import 'package:dev_track_app/components/topNav.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -16,9 +18,111 @@ class DomainPage extends StatelessWidget {
           children: [
             TopNav(),
             Container(
-              decoration: BoxDecoration(color: Color(0xFF183D3D)),
-              margin: EdgeInsets.only(left: 14,right: 14),
-            )
+              padding: EdgeInsets.all(10),
+              margin: EdgeInsets.only(left: 14, right: 14),
+              decoration: BoxDecoration(
+                color: Color(0xFF183D3D),
+                borderRadius: BorderRadius.circular(23),
+              ),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                        height: 70,
+                        width: 70,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(35),
+                            color: Color(0xFF5C8374)),
+                        child: Align(
+                          alignment: Alignment.center,
+                          child: Icon(
+                            Icons.pedal_bike,
+                            color: Colors.white,
+                            size: 38,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 4,
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "My Cycle",
+                            style: GoogleFonts.inter(
+                              textStyle: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 19,
+                                  fontWeight: FontWeight.w500),
+                            ),
+                          ),
+                          Text(
+                            "Web Dev 68",
+                            style: GoogleFonts.inter(
+                              textStyle: TextStyle(
+                                  color: Color(0xFFC6C6C6),
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w500),
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Text(
+                            'Week',
+                            style: GoogleFonts.inter(
+                              textStyle: TextStyle(
+                                fontSize: 21,
+                                color: Color(0xFF999999),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            width: 2,
+                          ),
+                          Text(
+                            '4',
+                            style: GoogleFonts.inter(
+                              textStyle: TextStyle(
+                                fontSize: 21,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      ElevatedButton(
+                        style: ButtonStyle(
+                            padding: MaterialStatePropertyAll(EdgeInsets.only(
+                                top: 20, bottom: 20, left: 45, right: 45)),
+                            backgroundColor:
+                                MaterialStatePropertyAll(Color(0xFF93B1A6)),
+                            foregroundColor:
+                                MaterialStatePropertyAll(Colors.white),
+                            textStyle: MaterialStatePropertyAll(TextStyle(
+                                fontSize: 19, fontWeight: FontWeight.bold))),
+                        child: Text("Review"),
+                        onPressed: null,
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+            SizedBox(height: 40,)
+            
           ],
         ),
       ),
