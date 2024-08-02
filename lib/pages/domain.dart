e2be28db146d345e1f90250eb95b6b10e43a76d9
Atul Reny <117465987+AtulRy@users.noreply.github.com>
2024-08-03 01:13:06 +0530

    top nav created

--- lib/pages/domain.dart
+++ lib/pages/domain.dart
@@ -1,10 +1,27 @@
+import 'package:dev_track_app/components/topNav.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class DomainPage extends StatelessWidget {
   const DomainPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Color(0xFF040D12),
+        body: Column(
+          children: [
+            TopNav(),
+            Container(
+              decoration: BoxDecoration(color: Color(0xFF183D3D)),
+              margin: EdgeInsets.only(left: 14,right: 14),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
