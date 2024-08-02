1d7ac7e01d761655a52b56fccbba6efaa1c2035c
Atul Reny <117465987+AtulRy@users.noreply.github.com>
2024-08-02 09:21:15 +0530

    testing branch protection

---
+++ lib/pages/domain.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DomainPage extends StatelessWidget {
+  const DomainPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
