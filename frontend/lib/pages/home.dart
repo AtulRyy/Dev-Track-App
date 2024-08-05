97d2c4eb3d0c46dd524c25276d7c1096adb8c9d1
Piyush Chakarborthy <102757301+Chakravartinsamrat@users.noreply.github.com>
2024-08-06 01:01:26 +0530

    Splash Screen
    Idli Dosa

--- frontend/lib/pages/home.dart
+++ frontend/lib/pages/home.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+
+import 'package:dev_track_app/pages/splashscreen.dart';
 import 'package:dev_track_app/pages/domain.dart';
 import 'package:flutter/material.dart';
 
@@ -14,35 +17,58 @@ class HomePage extends StatelessWidget {
           children: [
             ElevatedButton(
               style: ElevatedButton.styleFrom(
-               backgroundColor: Colors.purple,
-               foregroundColor: Colors.white,
+                backgroundColor: Colors.purple,
+                foregroundColor: Colors.white,
                 shadowColor: Colors.blueAccent, // Shadow color
                 elevation: 5, // Elevation of the button
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(30.0), // Rounded corners
                 ),
-                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15), // Padding inside the button 
+                padding: EdgeInsets.symmetric(
+                    horizontal: 20, vertical: 15), // Padding inside the button
                 textStyle: TextStyle(
                   fontSize: 18,
                   fontWeight: FontWeight.bold,
                 ),
               ),
               onPressed: () {
-                Navigator.push(
-                    context, MaterialPageRoute(builder: (context) => DomainPage()));
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => DomainPage()));
               },
               child: Text("Domain Page"),
             ),
             ElevatedButton(
               style: ElevatedButton.styleFrom(
-               backgroundColor: Colors.deepOrange,
-               foregroundColor: Colors.white,
+                backgroundColor: Colors.deepOrange,
+                foregroundColor: Colors.white,
                 shadowColor: Colors.blueAccent, // Shadow color
                 elevation: 5, // Elevation of the button
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(30.0), // Rounded corners
                 ),
-                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15), // Padding inside the button
+                padding: EdgeInsets.symmetric(
+                    horizontal: 20, vertical: 15), // Padding inside the button
+                textStyle: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => DomainPage()));
+              },
+              child: Text("Feed Page"),
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color.fromARGB(255, 50, 7, 120),
+                foregroundColor: Color.fromARGB(255, 242, 244, 244),
+                shadowColor: Colors.black12,
+                elevation: 5,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30.0),
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
                 textStyle: TextStyle(
                   fontSize: 18,
                   fontWeight: FontWeight.bold,
@@ -50,11 +76,10 @@ class HomePage extends StatelessWidget {
               ),
               onPressed: () {
                 Navigator.push(
-                    context, MaterialPageRoute(builder: (context) => DomainPage()));
+                    context, MaterialPageRoute(builder: (context) => Splash()));
               },
-              child: Text("Feed Page"),
-            ),
-
+              child: Text("SplashScreen"),
+            )
           ],
         ),
       ),
