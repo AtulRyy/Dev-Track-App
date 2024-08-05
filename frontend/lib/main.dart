fea51f584dd4b590eafb33ab3cdfb4d61415f5f6
Atul Reny <117465987+AtulRy@users.noreply.github.com>
2024-08-05 14:34:38 +0530

    homepage=>homepag

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Dev/Track',
       debugShowCheckedModeBanner: false,
-      home:  HomePage(),
+      home:  HomePag(),
     );
   }
 }
