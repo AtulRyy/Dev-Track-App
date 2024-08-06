5a9bd56d82cb7de093f2ee43291ec6986f70d4a7
bharathan06 <bharathan.m.blr@gmail.com>
2024-08-06 15:40:06 +0530

    file updates

--- backend/myproject/myproject/settings.py
+++ backend/myproject/myproject/settings.py
@@ -11,9 +11,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku
-import dj_database_url
-import os
+# import django_heroku
+# import dj_database_url
+# import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-kq3as+jr%-qb4eaw6!b51ca@2adbt4b^hdd-gap5dk*)&$wp!f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -89,8 +89,8 @@ DATABASES = {
     }
 }
 
-if 'DATABASE_URL' in os.environ:
-    DATABASES['default'] = dj_database_url.config(default=os.environ['DATABASE_URL'])
+# if 'DATABASE_URL' in os.environ:
+#     DATABASES['default'] = dj_database_url.config(default=os.environ['DATABASE_URL'])
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -126,10 +126,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS= (os.path.join(BASE_DIR, 'static'),)
-django_heroku.settings(locals())
+# STATIC_URL = 'static/'
+# STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles')
+# STATICFILES_DIRS= (os.path.join(BASE_DIR, 'static'),)
+# django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
