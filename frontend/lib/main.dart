dc2fd4cc87615928d32363f12ca6791194e3845a
Vidhi <vidhikamat01@gmail.com>
2024-08-06 15:08:05 +0530

    application page has been added

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,19 +1,21 @@
-import 'package:dev_track_app/pages/home.dart';
 import 'package:flutter/material.dart';
+import 'package:dev_track_app/pages/home_page.dart';
 
 void main() {
-  runApp(MyApp());
-
+  runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Dev/Track',
-      debugShowCheckedModeBanner: false,
-      home:  HomePage(),
+      title: 'Dev Track App',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: const HomePage(),
     );
   }
 }
