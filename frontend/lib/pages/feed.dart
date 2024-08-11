73a5038f7d2c0a6daec5608ed9e49e767ae3d6dc
Duke0fCodes <deek5h17h.c@gmail.com>
2024-08-11 15:18:35 +0530

    add new post
    floating button to add new post and store in temporary inbuilt data list

--- frontend/lib/pages/feed.dart
+++ frontend/lib/pages/feed.dart
@@ -11,15 +11,14 @@ class FeedPage extends StatefulWidget {
   @override
   State<FeedPage> createState() => _FeedPageState();
 }
-
+String usernam='AtulReny';
 class _FeedPageState extends State<FeedPage> {
   List<Data> posts = [
     Data(
       caption: 'Jackets are here!!!',
-      username: 'AtulReny',
+      username: usernam,
       content: 'Come and collect em today hehehehehe',
     ),
-    // Add more Data objects here as needed
   ];
   void _updateState() {
     setState(() {});
@@ -28,9 +27,19 @@ class _FeedPageState extends State<FeedPage> {
   void _deleteState() {
     setState(() {});
   }
+  void _addState(String cap, String con){
+    setState(() {
+      posts.insert(0,Data(caption: cap, username: usernam, content: con));
 
+    });
+  }
   @override
   Widget build(BuildContext context) {
+    TextEditingController _captionAdder = TextEditingController();
+    TextEditingController _contentAdder = TextEditingController();
+    String newcap='.';
+    String newcon='.';
+
     return SafeArea(
       child: Scaffold(
         backgroundColor: Color(0xFF040D12),
@@ -48,6 +57,72 @@ class _FeedPageState extends State<FeedPage> {
             ),
           ],
         ),
+          floatingActionButton: FloatingActionButton(onPressed: () {
+            showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return Dialog(
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(15),
+                  child: Container(
+                    padding: EdgeInsets.all(20),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        TextField(
+                          controller: _captionAdder,
+                          cursorColor: Colors.black45,
+                          style: TextStyle(color: Colors.black),
+                          decoration: InputDecoration(
+                            labelText: 'Add Caption',
+                            labelStyle: TextStyle(color: Colors.black45),
+                            enabledBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.black38),
+                            ),
+                            focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.black45),
+
+                            ),
+
+                          ),
+                        ),
+                        TextField(
+                          controller: _contentAdder,
+                          cursorColor: Colors.black45,
+                          style: TextStyle(color: Colors.black),
+                          decoration: InputDecoration(
+                            labelText: 'Add Content',
+                            labelStyle: TextStyle(color: Colors.black45),
+                            enabledBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.black38),
+                            ),
+                            focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.black45),
+
+                            ),
+
+                          ),
+                        ),
+                        SizedBox(height: 20),
+                        ElevatedButton(
+                          onPressed: () {
+                            newcap = _captionAdder.text;
+                            newcon = _contentAdder.text;
+                            _addState(newcap, newcon);
+                            Navigator.of(context).pop();
+                          },
+                          child: Text('Post',style: TextStyle(color: Colors.white),),
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Color(0xFF93B1A6), // Custom button color
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            },
+          );} ,backgroundColor: Color(0xFF93B1A6), child: Icon(CupertinoIcons.plus, color: Colors.white,),)
       ),
     );
   }
